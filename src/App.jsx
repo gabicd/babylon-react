dd6a150c18433ee0d1d0a66d78dfd2a30d798f67
gabicd <gabicd3323@gmail.com>
2025-08-19 13:25:07 -0300

    teste identificar qr code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useEffect } from 'react';
+import { Map, Marker } from 'maplibre-gl';
+import QrScanner from 'qr-scanner';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mapContainerRef = useRef(null);
+  const videoRef = useRef(null);
+  const scannerRef = useRef(null);
+
+  const assetData = {
+  entidade: {
+    id: 1,
+    nome: 'Teste',
+    descricao: 'Teste de entidade'
+  }
+
+};
+
+  useEffect(() => {
+    let map, marker;
+    if (mapContainerRef.current) {
+      map = new Map({
+        container: mapContainerRef.current,
+        style: 'https://demotiles.maplibre.org/style.json',
+        center: [-47.8833, -22.0167],
+        zoom: 3
+      });
+
+      marker = new Marker()
+        .setLngLat([-47.8833, -22.0167])
+        .addTo(map);
+
+      const markerElement = marker.getElement();  
+      markerElement.addEventListener('click', () => {
+        
+        if (!scannerRef.current && videoRef.current) {
+          scannerRef.current = new QrScanner(
+            videoRef.current,
+            result => {
+              console.log(`QR Code detected: ${result}`);
+
+              if(result.data == assetData.entidade.id) {
+                console.log('Asset found:', assetData.entidade);
+              }
+
+            },
+            {
+              highlightScanRegion: false,
+              highlightCodeOutline: false,
+            }
+          );
+        }
+        if (scannerRef.current) {
+          scannerRef.current.start().catch((error) => {
+            console.error('Error starting QR Scanner:', error);
+          });
+        }
+      });
+
+      return () => {
+      if (scannerRef.current) {
+        scannerRef.current.destroy();
+      }
+      if (map) map.remove();
+    };
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app-container">
+      <div ref={mapContainerRef} className="map-container" />
+    </div>
+    <div id="video-div">
+      <video ref={videoRef} id="qr-video"></video>
+    </div>
+
     </>
-  )
+
+  );
 }
 
-export default App
+export default App;
